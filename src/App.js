8f05e7f0744a90d0427adff76545ae6e309c4e37
Bekir PİRALP <bekir01piralp@gmail.com>
2025-05-18 20:06:19 +0300

    App dosyası fonksiyondan componente çevrildi

--- src/App.js
+++ src/App.js
@@ -1,31 +1,31 @@
-import React from "react";
+import React, {Component} from "react";
 // import { Navi, Product, Category } from "./components/imports";
 import Navi from "./components/Navi";
 import CategoryList from "./components/CategoryList";
 import ProductList from "./components/ProductList";
 import { Col, Container, Row } from "react-bootstrap";
 
-function App() {
-  let productInfo = {title:'Category List'}
-  let categoryInfo = {title:'Product List'}
-  return (
-    <div>
-      <Container>
-        <Row>
-          <Navi />
-        </Row>
+export default class App extends Component {
+  render() {
+    let productInfo = { title: "Category List" };
+    let categoryInfo = { title: "Product List" };
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Navi />
+          </Row>
 
-        <Row>
-          <Col xs="3">
-            <CategoryList info={categoryInfo} />
-          </Col>
-          <Col xs="9">
-            <ProductList info={productInfo} />
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+          <Row>
+            <Col xs="3">
+              <CategoryList info={categoryInfo} />
+            </Col>
+            <Col xs="9">
+              <ProductList info={productInfo} />
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
 }
-
-export default App;
